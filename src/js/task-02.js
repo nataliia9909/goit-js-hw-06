2250da7ab0da1111ed36cd443e9827dd56702d4b
nataliia9909 <113458747+nataliia9909@users.noreply.github.com>
2023-02-12 20:40:24 +0300

    added corrections of the mentor

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,25 +8,32 @@ const ingredients = [
 ];
 
 const listEl = document.querySelector('#ingredients');
-const items = [];
 
-for (const ingredient of ingredients) {
+// Розв'язок 1
+
+// const items = [];
+
+// for (const ingredient of ingredients) {
   
- const item = document.createElement("li");
- item.textContent = ingredient;
- item.classList.add("item");
- items.push(item);
-
-}
-
-listEl.append(...items);
-
-
-// Чому не працює через map?
-
-// const items = ingredients.map(ingredient => {
 //  const item = document.createElement("li");
 //  item.textContent = ingredient;
 //  item.classList.add("item");
-  
-// });
+//  items.push(item);
+
+// }
+
+// listEl.append(...items);
+
+
+// Чому не працює через map? Тепер працює через map, я забула return, дякую)
+
+// Розв'язок 2
+
+const items = ingredients.map(ingredient => {
+ const item = document.createElement("li");
+ item.textContent = ingredient;
+ item.classList.add("item");
+  return item
+});
+
+listEl.append(...items);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,13 +2,17 @@ const changeColorButtonEl = document.querySelector(".change-color");
 const nameOfColorEl = document.querySelector(".color");
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 
 const onChangeBacgroundColorOfBody = (e) => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  nameOfColorEl.textContent = document.body.style.backgroundColor;
+const randomColorBackground = document.body.style.backgroundColor;
+
+  randomColorBackground = getRandomHexColor();
+  nameOfColorEl.textContent = randomColorBackground;
 }
 
 changeColorButtonEl.addEventListener("click", onChangeBacgroundColorOfBody)
